146d5a1f3a1806714841e533fbf5c09d895aa2f6
pivotsk8 <the_pivotsk8@hotmail.com>
2022-02-04 21:37:10 -0500

    delete player 4/02/22

--- aws-la/src/getPlayerId.js
+++ aws-la/src/getPlayerId.js
@@ -1,23 +1,33 @@
 const AWS = require("aws-sdk");
 
 const getPlayerId = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-  var { Id } = event.pathParameters;
-  Id = parseInt(Id);
-  const result = await dynamodb
-    .get({
-      TableName: "CredituPlayers",
-      Key: {
-        Id,
-      },
-    })
-    .promise();
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    var { Id } = event.pathParameters;
+    Id = parseInt(Id);
+    const result = await dynamodb
+      .get({
+        TableName: "CredituPlayers",
+        Key: {
+          Id,
+        },
+      })
+      .promise();
 
-  const player = result.Item;
-  return {
-    status: 200,
-    body: player,
-  };
+    const player = result.Item;
+    return {
+      status: 200,
+      body: player,
+    };
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed GetPlayerId.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
 };
 
 module.exports = {
