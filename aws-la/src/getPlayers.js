864325029f295887590c00e8b5ff344f162c2c6b
pivotsk8 <the_pivotsk8@hotmail.com>
2022-02-06 16:35:59 -0500

    order player ranking

---
+++ aws-la/src/getPlayers.js
@@ -0,0 +1,115 @@
+const AWS = require("aws-sdk");
+
+const getPlayers = async (event) => {
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const result = await dynamodb
+      .scan({
+        TableName: "CredituPlayers",
+      })
+      .promise();
+
+    let players = result.Items;
+
+    if (event.queryStringParameters) {
+      const { nick_name, order, status, amount } = event?.queryStringParameters;
+
+      if (nick_name) {
+        if (/^\d+$/.test(nick_name)) {
+          let playerId = await dynamodb
+            .get({
+              TableName: "CredituPlayers",
+              Key: {
+                Id: parseInt(nick_name),
+              },
+            })
+            .promise();
+          if (playerId) {
+            playerId = playerId.Item;
+            players = [playerId];
+          } else {
+            players = players.filter((player) =>
+              player.nickname
+                .toLocaleLowerCase()
+                .includes(nick_name.toLocaleLowerCase())
+            );
+            if (players.length === 0) {
+              return {
+                status: 404,
+                body: [],
+              };
+            }
+          }
+        } else {
+          players = players.filter((player) =>
+            player.nickname
+              .toLocaleLowerCase()
+              .includes(nick_name.toLocaleLowerCase())
+          );
+          if (players.length === 0) {
+            return {
+              status: 404,
+              body: [],
+            };
+          }
+        }
+      }
+      if ((order && order === "asc") || order === "desc" || order === "") {
+        if (order === "asc" || order === "") {
+          // players.sort((a, b) => b.ranking - a.ranking);
+          players.sort((a, b) => a.ranking - b.ranking);
+        }
+        if (order === "desc") {
+          // players.sort((a, b) => a.ranking - b.ranking);
+          players.sort((a, b) => b.ranking - a.ranking);
+        }
+      }
+      if (
+        (status && status === "bronce") ||
+        status === "plata" ||
+        status === "oro" ||
+        status === "hierro"
+      ) {
+        if (status === "hierro") {
+          players = players.filter((player) => {
+            return player.status === "hierro";
+          });
+        }
+        if (status === "plata") {
+          players = players.filter((player) => {
+            return player.status === "plata";
+          });
+        }
+        if (status === "bronce") {
+          players = players.filter((player) => {
+            return player.status === "bronce";
+          });
+        }
+        if (status === "oro") {
+          players = players.filter((player) => {
+            return player.status === "oro";
+          });
+        }
+      }
+      if (amount) {
+        players = players.slice(0, parseInt(amount));
+      }
+    }
+    return {
+      status: 200,
+      body: { players },
+    };
+  } catch (e) {
+    console.error(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify({
+      message: "Failed to get players.",
+      errorMsg: e.message,
+      errorStack: e.stack,
+    });
+  }
+};
+
+module.exports = {
+  getPlayers,
+};
