2051969e46aed2cc3c84081d8dcdbfec6e2f7d3b
Gustavo Montero <79276560+gamontero@users.noreply.github.com>
2022-02-09 15:37:31 -0300

    Testing Back

--- cypress/integration/getPlayers.spec.js
+++ cypress/integration/getPlayers.spec.js
@@ -1,13 +1,12 @@
 const expect = require("chai").expect;
 
 describe('get players testing', () => {
-    it('fetches Todo items - GET', async () => {
+    it('obtiene todos los jugadores - GET', async () => {
         cy.request('/player').as('getPlayersRequest');
         await cy.get('@getPlayersRequest').then(response => {
             expect(response.status).to.eq(200);
-            console.log(response)
-            expect(response.body.body.players[0]).to.have.property('Id');
-            assert.isArray(response.body.body.players, 'Todos Response is an array')
+            expect((response.body.body.players[0]).to.have.property('Id'), 'Id se encuentra en la respuesta');
+            assert.isArray(response.body.body.players, 'Players es un array')
             
         });
     });
